bb6072b7958962daa5867d5b73070ecf0a3651f3
Otavio Ravagnani <otavio.ravagnani01@gmail.com>
2022-02-24 21:39:45 -0300

    nao finalizado

---
+++ Aula /ex01.py
@@ -0,0 +1,10 @@
+print(7+4)
+print('7' + '4')
+nome = 'Otavio'
+idade = 25
+peso = 80
+print(nome, idade, peso)
+nome = input('Qual seu nome ?')
+idade = input('Qual sua idade')
+peso = input('Qual seu peso ?')
+print(nome, idade, peso)
