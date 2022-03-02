2738a31be4693db1ec64796e7523f2008a9e1458
Otavio Ravagnani <otavio.ravagnani01@gmail.com>
2022-03-02 17:26:21 -0300

    finalizado

---
+++ Desafio/Ds02.py
@@ -0,0 +1,4 @@
+dia = input('dia')
+mes = input('mês')
+ano = input('ano')
+print('voçê nasceu no dia' ,dia, 'no mes' ,mes, 'e no ano' ,ano, 'correto')
---
+++ Desafio/Ds03.py
@@ -0,0 +1,3 @@
+n1 = input('Primeiro número')
+n2 = input('Segundo número')
+print('A soma dos dois números é', n1 + n2)
